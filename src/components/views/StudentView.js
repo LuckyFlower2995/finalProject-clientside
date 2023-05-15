badb908209e27f7fb4ea1fdf6b48e02c7b4644f5
Stephanie Lam <stephanielam@Stephanies-iMac.local>
2023-05-15 15:10:10 -0400

    made changes to view components

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,14 +4,27 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
+import { Link } from "react-router-dom";
+
 const StudentView = (props) => {
   const { student } = props;
 
   // Render a single Student view 
   return (
     <div>
-      <h1>First Name: {student.firstname + " " + student.lastname}</h1>
-      <h3>{student.campus.name}</h3>
+      <p>{student.imageURL}</p>
+      <h1>Full Name: {student.firstname + " " + student.lastname}</h1>
+      <p>Email: {student.email}</p>
+      <p>GPA: {student.gpa}</p>
+      
+      <div key={student.campus.id}>
+        <Link to={`/campus/${student.campus.id}`}>
+          <h2>Attends: {student.campus.name}</h2>
+        </Link>
+      </div>
+      
+    
+       
     </div>
   );
 
